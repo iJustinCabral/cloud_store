fe4f67778a4a7abdd2f08d2c9478fcbe73c7add1
iJustinCabral <ijustincabral@gmail.com>
2022-10-26 12:25:21 -0400

    Styled the landing page
    Styled the landing page to make it easy for the user to understand.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home.js'
 import Corporate from './pages/Corporate/Corporate'
 import Manager from './pages/Manager/Manager';
+import Customer from './pages/Customer/Customer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/corporate' element={<Corporate />} />
           <Route path='/manager' element={<Manager />} />
+          <Route path='/customer' element={<Customer />} />
         </Routes>
     </Router>
   );
