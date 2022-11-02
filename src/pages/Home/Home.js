3eae32af8d402af2f2a10b35fd9bac2b1d418d96
Justin Cabral <ijustincabral@gmail.com>
2022-11-02 11:07:21 -0400

    Removed Create Item Test
    Removed the create item test

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,22 +8,7 @@ const home = () => {
         <h1> Welcome To The Cloud Store </h1>
         <h4> Select your role below: </h4>
         <Stack direction="row" spacing={2}>
-            <Button onClick={() => {
-                fetch("https://0kh4satg9k.execute-api.us-east-1.amazonaws.com/default/create_item_lamda", {
-                    method: 'POST',
-                    headers: {
-                      'Accept': 'application/json',
-                      'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({ sku : "1239", name : "TestName", description : "Descr", price : 1, shelfMaxQty : 5}                    )
-                  })
-                  .then(response => response.json())
-                  .then(response => {
-                    console.log(response)
-                  })
-            }} variant="contained" color="primary">
-                Test API Gateway
-            </Button>
+          
             <Button component={Link} to="/login" variant="contained" color="primary">
                 Corporate
             </Button>
